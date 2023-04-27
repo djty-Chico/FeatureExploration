37fdb1de250cec608c8f73be4e078f3338d04cb9
djty-Chico <djty@mail.csuchico.edu>
2023-04-27 11:49:06 -0700

    input system dowload w/ materials & scripts

---
+++ FeatureExploration/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Variables
+
+    public float maxShootSpeed;
+    public float maxAmmo;
+    public float moveSpeed;
+    public float reloadSpeed;
+    public float crouchSpeed;
+    public float maxJumpHeight;
+    public float chargeTime;
+
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
