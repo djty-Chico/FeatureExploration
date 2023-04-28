40186ed575c34b1ca0e6ba5179f34a4dd17ccd99
djty-Chico <djty@mail.csuchico.edu>
2023-04-27 19:01:48 -0700

    setting up camera look & input system

--- FeatureExploration/Assets/Scripts/PlayerController.cs
+++ FeatureExploration/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Basic Movement Variables
+    [SerializeField]
+    private float walkSpeed = 10f;
 
+    private Vector2 moveInput;
+    private Rigidbody myRigidbody;
     //Variables
 
     public float maxShootSpeed;
@@ -19,12 +25,18 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerVelocity = new Vector3(moveInput.x * walkSpeed, myRigidbody.velocity.y, moveInput.y * walkSpeed);
+        myRigidbody.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
---
+++ FeatureExploration/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private float minViewDistance = 25f;
+    [SerializeField]
+    private Transform playerBody;
+
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, minViewDistance);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
